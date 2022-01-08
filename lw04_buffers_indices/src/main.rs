a5783dd604e4569c7f7116d2fcc0a29339cde41e
CatCode79 <andrea.postal@gmail.com>
2022-01-08 15:17:05 +0100

    intial lw04 example refactoring

--- lw04_buffers_indices/src/main.rs
+++ lw04_buffers_indices/src/main.rs
@@ -1,12 +1,8 @@
 //= USES ===========================================================================================
 
-use std::collections::HashMap;
-use std::fs::read_to_string;
-
 use wgpu::Color;
-use winit::dpi::PhysicalSize;
 
-use irid::{ApplicationBuilder, AppConfigBuilder, Listener};
+use irid::{ApplicationBuilder, Listener};
 use irid_assets::ColorVertex;
 
 //= GAME LOGIC =====================================================================================
@@ -14,23 +10,7 @@ use irid_assets::ColorVertex;
 struct GameListener {}
 
 impl Listener for GameListener {
-    fn on_suspend(&self) -> bool {
-        true
-    }
-
-    fn on_resume(&self) -> bool {
-        true
-    }
-
     fn on_redraw(&self) -> bool {
-        true
-    }
-
-    fn on_destroy(&self) -> bool {
-        true
-    }
-
-    fn on_window_resize(&self, _new_size: PhysicalSize<u32>) -> bool {
         true
     }
 }
@@ -41,33 +21,24 @@ fn main() {
     log::set_max_level(log::LevelFilter::Debug);
     env_logger::init();
 
-    let config = AppConfigBuilder::new()
-        .with_clear_color(Color {
-            r: 0.1,
-            g: 0.2,
-            b: 0.3,
-            a: 1.0,
-        })
-        .build();
-
-    let listener = &GameListener { };
-
-    const SHADER_WGSL_FILENAME: &str = "shader.wgsl";
-    const SHADER_WGSL_FILEPATH: &str = "D:/_BLACK_ABYSS_DUNGEON/_BAD/shaded_sun/lw_examples/lw04_buffers_indices/assets/shader.wgsl";
-
-    // TODO give path or filename only
-    let mut shaders = HashMap::new();
-    let frag_wgsl = match read_to_string(SHADER_WGSL_FILEPATH) {
-        Ok(file) => file.clone(),  // TODO try to remove the clone
-        Err(why) => panic!("couldn't open {} file: {}", SHADER_WGSL_FILENAME, why),
+    let clear_color = Color {
+        r: 0.1,
+        g: 0.2,
+        b: 0.3,
+        a: 1.0,
     };
-    shaders.insert(SHADER_WGSL_FILENAME.to_string(), frag_wgsl);
+
+    let listener = GameListener { };
+
+    let shader_paths = vec![
+        "lw04_buffers_indices/assets/shader.wgsl"
+    ];
 
     // We arrange the vertices in counter clockwise order: top, bottom left, bottom right.
     // We do it this way partially out of tradition, but mostly because we specified in the
     // rasterization_state of the render_pipeline that we want the front_face of our triangle
     // to be wgpu::FrontFace::Ccw so that we cull the back face.
-    const VERTICES: &[ColorVertex] = &[
+    let vertices: &[ColorVertex] = &[
         ColorVertex { position: [-0.08682410,  0.49240386, 0.0], colors: [0.5, 0.0, 0.5] },
         ColorVertex { position: [-0.49513406,  0.06958647, 0.0], colors: [0.5, 0.0, 0.5] },
         ColorVertex { position: [-0.21918549, -0.44939706, 0.0], colors: [0.5, 0.0, 0.5] },
@@ -75,13 +46,18 @@ fn main() {
         ColorVertex { position: [ 0.44147372,  0.23473590, 0.0], colors: [0.5, 0.0, 0.5] },
     ];
 
-    const INDICES: &[u16] = &[
+    let indices: &[u16] = &[
         0, 1, 4,
         1, 2, 4,
         2, 3, 4,
         /* padding */ 0,
     ];
 
-    let application = ApplicationBuilder::new_with_config(config).build();
-    application.start(listener);
+    let application = ApplicationBuilder::new(listener)
+        .with_clear_color(clear_color)
+        .with_shader_paths(shader_paths)
+        .with_vertices(vertices)
+        .with_indices(indices)
+        .build();
+    let _ = application.start();
 }
